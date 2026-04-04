5df2e157c64be0cab3b3a943bfd6727ca75cb883
Muhibmdn <muhib.ulum2003@gmail.com>
2026-04-04 20:16:54 +0800

    Use Different Data Types

--- js-basics/script.js
+++ js-basics/script.js
@@ -3,7 +3,23 @@ var name = "John";
 let age = 25;
 const pi = 3.14159;
 
-// Subtask 3.2 Display Variable Values
+// Subtask 2.2 Display Variable Values
 console.log("Name:", name);
 console.log("Age:", age);
 console.log("Pi:", pi);
+
+// Subtask 3.1 Use Different Data Types
+let angka = 10;
+let string = "Bismillah";
+let boolean = true;
+let array = [1, 2, 3, 4, 5];
+let object = {
+	nama: "Muhib",
+	umur: 22,
+	asal: "Madiun"
+}
+console.log("Angka:", angka);
+console.log("String:", string);
+console.log("Boolean:", boolean);
+console.log("Array:", array);
+console.log("Object:", object);
