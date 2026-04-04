1ab9b4b65ae132ce01304cb145b361a946816538
Muhibmdn <muhib.ulum2003@gmail.com>
2026-04-04 22:47:21 +0800

    Subtask 2.1 Add condition logic in script.js

---
+++ js-controlFlow/script.js
@@ -0,0 +1,13 @@
+// Subtask 2.1 Add condition logic in script.js
+let nilai = 80.9
+if (nilai >= 81) {
+	console.log("Nilai A")
+}else if (nilai >= 61) {
+	console.log("Nilai B")
+}else if (nilai >= 41) {
+	console.log("Nilai C")
+}else if (nilai >= 21) {
+	console.log("Nilai D")
+}else {
+	console.log("Nilai E")
+}
