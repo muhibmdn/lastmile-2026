da9d2ec7a456a5507beeaf1426255218325cec5d
Muhibmdn <muhib.ulum2003@gmail.com>
2026-04-04 20:15:10 +0800

    Declare n Display Variable Values

---
+++ js-basics/script.js
@@ -0,0 +1,9 @@
+// Subtask 2.1 Declare Variables
+var name = "John";
+let age = 25;
+const pi = 3.14159;
+
+// Subtask 3.2 Display Variable Values
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Pi:", pi);
