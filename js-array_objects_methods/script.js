30919b235c533794b31701654f202e83fc0eb02e
Muhibmdn <muhib.ulum2003@gmail.com>
2026-04-05 23:58:26 +0800

    Subtask 2.2 Use Array Methods

---
+++ js-array_objects_methods/script.js
@@ -0,0 +1,28 @@
+// Subtask 2.2 Use Array Methods
+let angka = [1, 2, 3, 4, 5];
+let nama = ["Muhib", "Ahmad", "Wildan", "Fawas"];
+let daftarStatus = [true, false, true, false];
+let campuran = [1, "Muhib", true, 3.14];
+let matriks = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9],
+];
+let siswa = [
+	{"nama": "Muhib", "umur": 20},
+	{"nama": "Ahmad", "umur": 22},
+	{"nama": "Wildan", "umur": 21},
+	{"nama": "Fawas", "umur": 19},
+];
+let salam = [
+	() => console.log("Halo"),
+	() => console.log("Assalamu'alaikum"),
+];
+salam[1]();
+
+console.log(angka);
+console.log(nama);
+console.log(daftarStatus);	
+console.log(campuran);
+console.log(matriks);
+console.log(siswa);	
